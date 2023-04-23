174d40b109fec1a017b92d902c2d0bdcee41ca65
Zwea Htet <zhtet5@berkeley.edu>
2023-04-22 17:47:06 -0700

    fixed import error

--- data/prepare.py
+++ data/prepare.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_json(r"./regItems.json")
+df = pd.read_json(r"data/regItems.json")
 df = df.replace(to_replace="", value=np.nan).dropna(axis=0) # remove null values
 df['paragraphText'] = df['paragraphText'].str.replace("OLD SECTION.*", "", regex=True) # remove any dirty words
 # df['paragraphText'] = df['paragraphText'].str.replace("[a-zA-z]\d\w+", ". ", regex=True)
